769950b9d61677c0aa9baf30d311a014db4edb06
Anirbandasjoy <joy600508@gmail.com>
2025-08-16 19:33:04 +0600

    update error handling

--- src/utils/loginLimiter.ts
+++ src/utils/loginLimiter.ts
@@ -1,7 +1,18 @@
 import rateLimit from 'express-rate-limit';
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 export const loginLimiter = rateLimit({
   windowMs: 10 * 60 * 1000,
   max: 5,
-  message: 'Too many login attempts. Try again after 10 minutes.',
+  standardHeaders: true,
+  legacyHeaders: false,
+
+  handler: (_req: Request, _res: Response, next: NextFunction) => {
+    const error = {
+      statusCode: StatusCodes.TOO_MANY_REQUESTS,
+      message: 'Too many login attempts. Try again after 10 minutes.',
+    };
+    next(error);
+  },
 });
